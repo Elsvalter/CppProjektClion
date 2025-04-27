744fd7ce8ae35b8eb4a3e4b0c76553377837a3bd
hellrand <beatricemarj@gmail.com>
2025-04-27 23:25:36 +0300

    added .gitignore

---
+++ cmake-build-debug/CMakeFiles/ProjektVol2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjektVol2.dir/Task.cpp.o"
+  "CMakeFiles/ProjektVol2.dir/Task.cpp.o.d"
+  "CMakeFiles/ProjektVol2.dir/TodoListManager.cpp.o"
+  "CMakeFiles/ProjektVol2.dir/TodoListManager.cpp.o.d"
+  "CMakeFiles/ProjektVol2.dir/main.cpp.o"
+  "CMakeFiles/ProjektVol2.dir/main.cpp.o.d"
+  "ProjektVol2"
+  "ProjektVol2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjektVol2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
