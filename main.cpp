e11c2a7d1b1578abc9bf34ca2d7e927533356937
elsvalte <elisabeth.helena.valter@ut.ee>
2025-04-27 23:13:04 +0300

    More methods for TodoListManager

--- main.cpp
+++ main.cpp
@@ -4,30 +4,16 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include "TodoListManager.h"
+#include "Task.h"
+using namespace std;
 
-int main(int argc, char *argv[])
-{
-    std::string name{};
-    int day, month, year;
 
-    std::cout << "Welcome to your to-do list!\n";
-    std::cout << "Enter new task name: ";
-    std::getline(std::cin, name);  // Use getline in case the task name contains spaces
+int main(int argc, char *argv[]){
+    TodoListManager manager;
+    std::string filename = "tasks.txt";
 
-    std::cout << "Enter deadline (day month year): ";
-    std::cin >> day >> month >> year;
+    bool running = true;
 
-    Task task(name, day, month, year);
 
-    std::cout << "\n" << task << std::endl;
-    int daysLeft = task.timeToComplete();
-
-    if (daysLeft > 0)
-        std::cout << daysLeft << " day(s) until deadline.\n";
-    else if (daysLeft == 0)
-        std::cout << "The deadline is today!\n";
-    else
-        std::cout << "The deadline has passed by " << -daysLeft << " day(s).\n";
-
-    return 0;
 }
